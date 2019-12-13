783a23c92101636bf8a58a728ec9a6e55a804014
Roman <romanm@paulbunyan.net>
2019-12-13 12:04:01 -0600

    ivanov numbers

---
+++ config/numbers.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    "12182805085", // Me
+    "15034386036", // Alia
+    "14063811446", // Mom
+    "12182807450", // Suzy
+    "16125996516", // Aunt Elena
+    "16126166965", // Aunt Liza
+    "15035511775", // Aunt Domnica
+    "15038812338", // Frosia
+    "12182805123", // Alosha
+    "16122697471", // Aunt Anastasia
+    "15039496070", // Aunt Hina
+    "19079829787", // Aunt Vassa
+    "15038052790", // Aunt Vivea
+    "19072991404", // Aunt Walle
+    "15035807273", // Paul
+    "15032015866", // Uncle Mike
+    "15035801961", // Uncle Pete
+    "17632386169", // Uncle Savva
+    "16126553822", // Uncle Steve
+    "12184310214", // Vanner Banner
+    "19073398512", // Aunt Alla
+    "12185210677", // Julie
+    "15039848010", // Seanna
+    "16129685103", // Aunt Suzanna
+    "16127101558", // Aunt Zina
+    "12182808511", // Baba
+    "12182802262", // Deda
+    "12182800489", // Emela
+];
